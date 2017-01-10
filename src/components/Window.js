2a76f64b0fc4a461078c527170843d22a33a2360
Yossi Eynav <yossi.eynav@gmail.com>
2017-01-10 11:54:46 +0200

    First commit

---
+++ src/components/Window.js
@@ -0,0 +1,7 @@
+import 'react';
+
+const Window = () => {
+    return (<h1> Headline </h1>)
+}
+
+export default Window;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import Immutable from 'immutable';
+
+const initialState = Immutable.Map();
+                            
+const reducers = (state = initialState , action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+};
+
+export default reducers;
